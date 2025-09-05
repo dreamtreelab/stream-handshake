615f7b169d22b0e794f89869f09a4b30cab467f0
dreamtreelab <89558750+dreamtreelab@users.noreply.github.com>
2025-09-05 17:42:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,32 @@ const io = new Server(server, {
   cors: { origin: "*" } // allow browser connections
 });
 
+// Track connected Unity clients and browsers
+let unityClients = {}; // key: socket.id, value: { type: 'unity' }
+let browsers = {};     // key: socket.id, value: { type: 'browser' }
+
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
 
-  // Forward signaling messages
+  socket.on('register', (data) => {
+    if (data.type === 'unity') {
+      unityClients[socket.id] = socket;
+      console.log('Registered Unity client:', socket.id);
+    } else if (data.type === 'browser') {
+      browsers[socket.id] = socket;
+      console.log('Registered Browser:', socket.id);
+
+      // notify Unity of new browser
+      const unityIds = Object.keys(unityClients);
+      if (unityIds.length > 0) {
+        // for simplicity, pick first Unity instance
+        const unitySocket = unityClients[unityIds[0]];
+        unitySocket.emit('new_browser', { browserId: socket.id });
+      }
+    }
+  });
+
+  // Relay SDP/ICE between Unity and browser
   socket.on('signal', (data) => {
     const { target, signal } = data;
     if (io.sockets.sockets.get(target)) {
@@ -21,6 +43,8 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
+    delete unityClients[socket.id];
+    delete browsers[socket.id];
   });
 });
 
